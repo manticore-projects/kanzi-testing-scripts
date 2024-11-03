28d013b77a7141b94df2d59f02839b3902fe0494
Ulrik Dickow <u.dickow@gmail.com>
2024-11-03 21:34:46 +0100

    Use 64M blocks in kanzi & 7za ppmd loops and for zpaq
    Use 64 megabyte block size instead of 8 MiB in several places:
    
      * in the kanzi 1/2/3 level transform loops
      * in the 7za ppmd order 2-to-32 loop
      * in an extra zpaq test
    
    For kanzi and 7-zip this often reduces compressed size even for input
    files much smaller than 64 MiB.
    
    It also makes the script more useful for input files larger than 8 MiB.

--- size-kanzi-algos-etc.sh
+++ size-kanzi-algos-etc.sh
@@ -4,6 +4,9 @@
 #   mostly trying lots of different transform-entropy kombis for kanzi in parallel after initial non-parallel
 #   testing of a few other compressors and the level 1-9 kanzi presets.
 #   This script is _not_ for speed testing.  Each compression is single-threaded.
+# Changes:
+#   ukd v02 2024-11-03: Add zpaq with 64M buffer and change parallel kanzi blocksize to 64M since it can increase
+#                       compression ratio by as much as 2.6% going from 8m to 16m even for only 3.6m input
 
 if [ $# -ne 1 -o ! -r "$1" ]; then
     echo "Usage: $0 FILE"
@@ -30,15 +33,17 @@ printf '%9d lrzip -p1 \n'  $(lrzip -p1 -qo- "$ifile"|wc -c)
 printf '%9d lrzip -zp1 \n' $(lrzip -zp1 -qo- "$ifile"|wc -c)
 printf '%9d zpaq a ... -m53 -t1\n' \
        $(t=`mktemp` && zpaq a $t "$ifile" -m53 -t1 >& /dev/null && wc -c < $t && rm $t)
+printf '%9d zpaq a ... -m56 -t1\n' \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m56 -t1 >& /dev/null && wc -c < $t && rm $t)
 printf '%9d arj\n' \
        $(t=`mktemp -u` && arj a $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 printf '%9d 7za a ... -m0=ppmd -mmt1\n' \
        $(t=`mktemp -u` && 7za a -m0=ppmd -mmt1 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 
-# 7za ppmd order 2 to 32 (entire valid range)(and force buffer size 8 MiB)
+# 7za ppmd order 2 to 32 (entire valid range)(and force buffer size 64 MiB)
 for o in {2..32}; do
-    printf "%9d 7za a ... -m0=ppmd:mem=23:o=$o -mmt1\n" \
-	   $(t=`mktemp -u` && 7za a -m0=ppmd:mem=23:o=$o -mmt1 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
+    printf "%9d 7za a ... -m0=ppmd:mem=26:o=$o -mmt1\n" \
+	   $(t=`mktemp -u` && 7za a -m0=ppmd:mem=26:o=$o -mmt1 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 done
 
 # kanzi level 1 to 9 
@@ -46,30 +51,30 @@ for level in {1..9}; do
     printf "%9d kanzi -l $level -j 1\n" $(kanzi -c -l $level -j 1 -i "$ifile" -o stdout|wc -c)
 done
 
-# kanzi with one transform (including NONE) and one entropy coding, testing 8 in parallel, forcing blocksize 8 MiB
+# kanzi with one transform (including NONE) and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
 #   (for 2+ transforms we will drop the last 3 in trans_list)
 trans_list="NONE PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA MM UTF EXE"
 entropy_list="NONE HUFFMAN ANS0 ANS1 RANGE CM FPAQ TPAQ TPAQX"
 for t1 in $trans_list; do
     for e in $entropy_list; do
-	echo -t $t1 -e $e
+	echo -b 64m -t $t1 -e $e
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 -b 8m {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
 
-# kanzi with two non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 8 MiB
+# kanzi with two non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
 trans_list="PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA"
 for t1 in $trans_list; do
     for t2 in $trans_list; do
 	# Two identical transforms in a row isn't useful, skip that
 	if [ $t1 != $t2 ]; then
 	    for e in $entropy_list; do
-		echo -t ${t1}+${t2} -e $e
+		echo -b 64m -t ${t1}+${t2} -e $e
 	    done
 	fi
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 -b 8m {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
 
-# kanzi with 3 non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 8 MiB
+# kanzi with 3 non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
 trans_list="PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA"
 for t1 in $trans_list; do
     for t2 in $trans_list; do
@@ -79,10 +84,10 @@ for t1 in $trans_list; do
 		# Two identical transforms in a row isn't useful, skip that
 		if [ $t2 != $t3 ]; then
 		    for e in $entropy_list; do
-			echo -t ${t1}+${t2}+${t3} -e $e
+			echo -b 64m -t ${t1}+${t2}+${t3} -e $e
 		    done
 		fi
 	    done
 	fi
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 -b 8m {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
