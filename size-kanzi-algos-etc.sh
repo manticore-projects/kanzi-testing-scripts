2631584c604847cb4a8fca865d221c7b1e9c65ef
Ulrik Dickow <u.dickow@gmail.com>
2025-01-02 21:14:38 +0100

    Add tests of up to 256M blocks; allow override of NJOBS; ...
    Add more improvements to the size testing script:
      * Add tests of bzip, zpaq and others with up to 256M block size
      * Allow override of the amount of parallelism (number of jobs)
      * Let some of the initial non-looped tests use multi-threading
      * Test MM transform instead of DNA in the 2+ transform loops
      * Add more xz(1) variants, e.g. also tuned for xwd(1) dumps (delta 4)
      * Add 256M kanzi tests targeted at typical journalctl(1) log dumps
    
    The 256M block size makes it useful for e.g. silesia.tar too.
    
    (Minor bug in this version: the 4 in text "-b  96m -j 4", should be 3).

--- size-kanzi-algos-etc.sh
+++ size-kanzi-algos-etc.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 # Author: Ulrik Dickow <udickow@gmail.com>, 2024-11-03.
-# Purpose: print lines with compressed size and algorithm for given input file (at most ~8 MiB recommended),
+# Purpose: print lines with compressed size and algorithm for given input file,
 #   mostly trying lots of different transform-entropy kombis for kanzi in parallel after initial non-parallel
 #   testing of a few other compressors and the level 1-9 kanzi presets.
-#   This script is _not_ for speed testing.  Each compression is single-threaded.
+#   This script is _not_ for speed testing.  The parallelly looped kanzis are single-threaded.
 # Changes:
+#   ukd v04 2025-01-02: Add more block sizes, more multi-threading before GNU parallel loops, more flexibility,
+#                       MM instead of DNA in 2+-level loops, ...
 #   ukd v03 2024-11-30: Add kanzi -l X with blocksize 64M too; add -x64 to all kanzi; add zpaq -m46; add xz 9e;
 #                       add EXE transform to the parallel kanzi tests too
 #   ukd v02 2024-11-03: Add zpaq with 64M buffer and change parallel kanzi blocksize to 64M since it can increase
@@ -14,6 +16,11 @@ if [ $# -ne 1 -o ! -r "$1" ]; then
     echo "Usage: $0 FILE"
     exit 1
 fi
+
+# Number of parallel kanzis in 2+-level loops; allow override via environment, default half number of processors.
+# Also used in a few initial multi-threaded compressions.
+NJOBS=${NJOBS:-$(($(nproc) / 2))}
+
 # export to be used by GNU parallel later
 export ifile="$1"
 
@@ -27,29 +34,48 @@ printf '%9d zstd --ultra -22\n' $(zstd --ultra -22c "$ifile"|wc -c)
 printf '%9d bzip2\n' $(bzip2 -c "$ifile"|wc -c)
 printf '%9d xz\n'    $(xz -c "$ifile"|wc -c)
 printf '%9d xz -e\n' $(xz -ec "$ifile"|wc -c)
+printf '%9d xz -9e\n' $(xz -9ec "$ifile"|wc -c)
+printf '%9d xz --lzma2=preset=9e,dict=256MiB\n' $(xz -c --lzma2=preset=9e,dict=256MiB "$ifile"|wc -c)
 printf '%9d xz --lzma2=preset=6,lc=4,pb=0\n' $(xz -c --lzma2=preset=6,lc=4,pb=0 "$ifile"|wc -c)
 printf '%9d xz --lzma2=preset=6e,lc=4,pb=0\n' $(xz -c --lzma2=preset=6e,lc=4,pb=0 "$ifile"|wc -c)
 printf '%9d xz --lzma2=preset=9e,lc=4,pb=0\n' $(xz -c --lzma2=preset=9e,lc=4,pb=0 "$ifile"|wc -c)
-printf '%9d bzip3\n' $(bzip3 -c "$ifile"|wc -c)
+printf '%9d xz --lzma2=preset=9e,lc=4,pb=0,dict=256MiB\n' $(xz -c --lzma2=preset=9e,lc=4,pb=0,dict=256MiB "$ifile"|wc -c)
+printf '%9d xz --delta=dist=4 --lzma2=preset=7e,lc=4\n'   $(xz -c --delta=dist=4 --lzma2=preset=7e,lc=4 "$ifile"|wc -c)
+printf '%9d bzip3\n' $(bzip3 -c -j$NJOBS "$ifile"|wc -c)
+printf '%9d bzip3 -b32\n' $(bzip3 -c -j$NJOBS -b32 "$ifile"|wc -c)
+printf '%9d bzip3 -b64\n' $(bzip3 -c -j$NJOBS -b64 "$ifile"|wc -c)
+printf '%9d bzip3 -b128\n' $(bzip3 -c -b128 "$ifile"|wc -c)
+printf '%9d bzip3 -b256\n' $(bzip3 -c -b256 "$ifile"|wc -c)
 
 printf '%9d lrzip -p1 \n'  $(lrzip -p1 -qo- "$ifile"|wc -c)
 printf '%9d lrzip -zp1 \n' $(lrzip -zp1 -qo- "$ifile"|wc -c)
-printf '%9d zpaq a ... -m46 -t1\n' \
-       $(t=`mktemp` && zpaq a $t "$ifile" -m46 -t1 >& /dev/null && wc -c < $t && rm $t)
-printf '%9d zpaq a ... -m53 -t1\n' \
-       $(t=`mktemp` && zpaq a $t "$ifile" -m53 -t1 >& /dev/null && wc -c < $t && rm $t)
-printf '%9d zpaq a ... -m56 -t1\n' \
-       $(t=`mktemp` && zpaq a $t "$ifile" -m56 -t1 >& /dev/null && wc -c < $t && rm $t)
+printf '%9d zpaq a ... -m46\n' \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m46 >& /dev/null && wc -c < $t && rm $t)
+printf '%9d zpaq a ... -m53\n' \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m53 >& /dev/null && wc -c < $t && rm $t)
+printf "%9d zpaq a ... -m56 -t$NJOBS\n" \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m56 -t$NJOBS >& /dev/null && wc -c < $t && rm $t)
+printf "%9d zpaq a ... -m57 -t$NJOBS\n" \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m57 -t$NJOBS >& /dev/null && wc -c < $t && rm $t)
+printf "%9d zpaq a ... -m58 -t$NJOBS\n" \
+       $(t=`mktemp` && zpaq a $t "$ifile" -m58 -t$NJOBS >& /dev/null && wc -c < $t && rm $t)
 printf '%9d arj\n' \
        $(t=`mktemp -u` && arj a $t "$ifile" > /dev/null && wc -c < $t && rm $t)
-printf '%9d 7za a ... -m0=ppmd -mmt1\n' \
-       $(t=`mktemp -u` && 7za a -m0=ppmd -mmt1 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
+printf '%9d 7za a -m0=ppmd\n' \
+       $(t=`mktemp -u` && 7za a -m0=ppmd $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 
 # 7za ppmd order 2 to 32 (entire valid range)(and force buffer size 64 MiB)
 for o in {2..32}; do
-    printf "%9d 7za a ... -m0=ppmd:mem=26:o=$o -mmt1\n" \
-	   $(t=`mktemp -u` && 7za a -m0=ppmd:mem=26:o=$o -mmt1 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
+    printf "%9d 7za a -m0=ppmd:mem=26:o=$o\n" \
+	   $(t=`mktemp -u` && 7za a -m0=ppmd:mem=26:o=$o $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 done
+
+# 7za ppmd order 14 with buffer size 256 MiB
+printf "%9d 7za a -m0=ppmd:mem=28:o=14\n" \
+       $(t=`mktemp -u` && 7za a -m0=ppmd:mem=28:o=14 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
+# 7za ppmd order 32 with buffer size 256 MiB
+printf "%9d 7za a -m0=ppmd:mem=28:o=32\n" \
+       $(t=`mktemp -u` && 7za a -m0=ppmd:mem=28:o=32 $t "$ifile" > /dev/null && wc -c < $t && rm $t)
 
 # kanzi level 1 to 9 with the default blocksize for each level (higher levels default to larger sizes)
 for level in {1..9}; do
@@ -61,18 +87,28 @@ for level in {1..9}; do
     printf "%9d kanzi -x64 -b 64m -l $level -j 1\n" $(kanzi -c -x64 -b 64m -l $level -j 1 -i "$ifile" -o stdout|wc -c)
 done
 
+# kanzi larger blocks for level 9 and custom transforms (suitable for e.g. large systemd journalctl(1) outputs)
+printf "%9d kanzi -x64 -b  96m -l 9 -j 4\n" $(kanzi -c -x64 -b  96m -l 9 -j 3 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 128m -l 9 -j 2\n" $(kanzi -c -x64 -b 128m -l 9 -j 2 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 256m -l 9 -j 1\n" $(kanzi -c -x64 -b 256m -l 9 -j 1 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 256m -t RLT+PACK       -e TPAQX -j 1\n" $(kanzi -c -x64 -b 256m -t RLT+PACK       -e TPAQX -j 1 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 256m -t RLT+TEXT+PACK  -e TPAQX -j 1\n" $(kanzi -c -x64 -b 256m -t RLT+TEXT+PACK  -e TPAQX -j 1 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 256m -t TEXT+ZRLT+PACK -e TPAQX -j 1\n" $(kanzi -c -x64 -b 256m -t TEXT+ZRLT+PACK -e TPAQX -j 1 -i "$ifile" -o stdout|wc -c)
+printf "%9d kanzi -x64 -b 256m -t TEXT+RLT+PACK  -e TPAQX -j 1\n" $(kanzi -c -x64 -b 256m -t TEXT+RLT+PACK  -e TPAQX -j 1 -i "$ifile" -o stdout|wc -c)
+
 # kanzi with one transform (including NONE) and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
 #   (for 2+ transforms we will drop the last 2 in trans_list)
-trans_list="NONE PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA EXE MM UTF"
+trans_list="NONE PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT EXE MM UTF DNA"
 entropy_list="NONE HUFFMAN ANS0 ANS1 RANGE CM FPAQ TPAQ TPAQX"
 for t1 in $trans_list; do
     for e in $entropy_list; do
 	echo -x64 -b 64m -t $t1 -e $e
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i "$ifile" -o stdout|wc -c)'
+
+trans_list="PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT EXE MM"
 
 # kanzi with two non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
-trans_list="PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA EXE"
 for t1 in $trans_list; do
     for t2 in $trans_list; do
 	# Two identical transforms in a row isn't useful, skip that
@@ -82,10 +118,9 @@ for t1 in $trans_list; do
 	    done
 	fi
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j$NJOBS 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i "$ifile" -o stdout|wc -c)'
 
 # kanzi with 3 non-null transforms and one entropy coding, testing 8 in parallel, forcing blocksize 64 MiB
-trans_list="PACK BWT BWTS LZ LZX LZP ROLZ ROLZX RLT ZRLT MTFT RANK SRT TEXT DNA EXE"
 for t1 in $trans_list; do
     for t2 in $trans_list; do
 	# Two identical transforms in a row isn't useful, skip that
@@ -100,4 +135,4 @@ for t1 in $trans_list; do
 	    done
 	fi
     done
-done | parallel -j8 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i $ifile -o stdout|wc -c)'
+done | parallel -j$NJOBS 'printf "%9d kanzi {=uq=}\n" $(kanzi -c -j 1 {=uq=} -i "$ifile" -o stdout|wc -c)'
